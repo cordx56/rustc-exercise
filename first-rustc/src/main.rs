1658c350675ea6eff988762a940aaf035b6013ef
CHISEN Kaoru <cordx56@cordx.cx>
2025-03-13 23:01:58 +0000

    書き加え

--- first-rustc/src/main.rs
+++ first-rustc/src/main.rs
@@ -1,3 +1,22 @@
+#![feature(rustc_private)]
+
+extern crate rustc_driver;
+extern crate rustc_interface;
+extern crate rustc_middle;
+
+struct MyRustc;
+impl rustc_driver::Callbacks for MyRustc {
+    fn after_analysis<'tcx>(
+        &mut self,
+        _compiler: &rustc_interface::interface::Compiler,
+        _tcx: rustc_middle::ty::TyCtxt<'tcx>,
+    ) -> rustc_driver::Compilation {
+        println!("analyze finished!");
+        rustc_driver::Compilation::Continue
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = std::env::args().collect();
+    rustc_driver::run_compiler(&args, &mut MyRustc);
 }
